838c60a3a49c0e4fa53eb208e9b4699fd6c1b9b9
sunglyoungKim <31963223+sunglyoungKim@users.noreply.github.com>
2018-05-03 21:31:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask, Response
 # from analysis import loadData, createChart, showTopWords
 from analysis import loadData, createChart
 
-data = loadData()
+#  = loadData(zipcode)
 app = Flask(__name__, static_url_path='', static_folder='.')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
-@app.route('/vis/<z>')
-def visualize(platform, total):
-    df = data.get(platform, None)
+@app.route('/vis/<zipcode>')
+def visualize(zipcode):
+    df = loadData(zipcode)
     response = ''
     if df is not None:
         #response = showTopWords(df[df.rating==rating]['content']).to_json()
-        response = createChart(df[['name', 'total']], platform).to_json()
+        response = createChart(df, zipcode).to_json()
 
     return Response(response,
         mimetype='application/json',
